f3ba716bf077f8560d1da276748bad20b97ac1c3
Odoriko <hoangle8122003@gamil.com>
2026-04-26 18:56:54 +0700

    change context

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,23 @@ type Params = {
   };
 };
 
+// 👇 THÊM CÁI NÀY
+export function generateStaticParams() {
+  return [
+    { slug: "post-1" },
+    { slug: "post-2" },
+  ];
+}
+
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>This is the content for the post with slug:</p></>;
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>This is the content for the post with slug:</p>
+    </>
+  );
 }
